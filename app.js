c5e131f0ef9562736f8451713760a9e4e08e6a7d
Petar Grchevski <petar.numb@gmail.com>
2018-02-12 14:17:27 +0100

    added npm start

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 var express = require('express')
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
 var app = express()
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'me',
-  password: 'secret',
-  database: 'my_db'
-});
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'me',
+//   password: 'secret',
+//   database: 'my_db'
+// });
 
-connection.connect();
+// connection.connect();
 
-
- 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/test_mysql_connection', function(req,res){
-  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error){
-      res.statusCode = 500;
-      res.send(error);
-    }
-    res.send(results);
-  });
+app.get('/goodbye', function(req, res){
+  res.send('Goodbye World')
 })
+
+// app.get('/test_mysql_connection', function(req,res){
+//   connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error){
+//       res.statusCode = 500;
+//       res.send(error);
+//     }
+//     res.send(results);
+//   });
+// })
  
-app.listen(3000)
+app.listen(4000)
